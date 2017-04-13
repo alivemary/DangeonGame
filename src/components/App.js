511a9156497661e02ab18e8c6f7a89fa3bd01332
alivemary <alivemary@gmail.com>
2017-04-13 11:41:21 +0200

    git support

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import DungeonGame from './DungeonGame';
 
 class App extends React.Component {
-	render() {
+    render() {
         return (
-					<div>
-					<div className="app-header">
-					 <h2>Roguelike Dungeon Crawler Game</h2>
-					</div>
-           <DungeonGame />
-					</div>
+            <div>
+                <div className="app-header">
+                  <h2>Roguelike Dungeon Crawler Game</h2> 
+                </div> 
+                <DungeonGame />
+            </div>
         )
     }
 }
