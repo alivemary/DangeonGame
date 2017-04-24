071bbd738f1d464c1435f6f4770472410dfd7777
alivemary <alivemary@gmail.com>
2017-04-24 22:17:46 +0200

    images added

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var path = require('path');
+var path = require("path");
 
 module.exports = {
   entry: "./src/app.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: "app.bundle.js"
   },
   module: {
@@ -14,14 +13,32 @@ module.exports = {
       {
         test: /.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
         })
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            query: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              }
+            }
+          }
+        ]
       }
     ]
   },
@@ -29,18 +46,18 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
-    stats: 'errors-only',
+    stats: "errors-only",
     open: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Roguelike Dungeon Crawler Game',
+      title: "Roguelike Dungeon Crawler Game",
       minify: {
         collapseWhitespace: true
       },
       hash: true,
-      template: './src/index.html'
+      template: "./src/index.html"
     }),
     new ExtractTextPlugin("styles.css")
   ]
-}
+};
